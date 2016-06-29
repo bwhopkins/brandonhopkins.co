df19b4e67dad1dbd81cc7af03085ebf7a67a9d33
Zachary J Rollyson <zach@izimobile.com>
2016-06-29 01:26:17 -0400

    Footer styling.

--- app/containers/Footer.jsx
+++ app/containers/Footer.jsx
@@ -1,18 +1,35 @@
 import React, { Component, PropTypes } from 'react';
 
+require('../sass/containers/footer.scss');
+
+const behanceImg = require('../images/behance@2x.png');
+const dribbleImg = require('../images/dribbble@2x.png');
+const twitterImg = require('../images/twitter@2x.png');
+
 class Footer extends Component {
 
   render() {
     return (
-      <footer role="footer">
-        <div>
-          <span>Copyright 2016 Brandon Hopkins Studio</span> | <a href="#">Contact</a>
-        </div>
-        <div>
-          <a href="#"><img src="http://placehold.it/25" alt="Behance" /></a>
-          <a href="#"><img src="http://placehold.it/25" alt="Dribble" /></a>
-          <a href="#"><img src="http://placehold.it/25" alt="Facebook" /></a>
-          <a href="#"><img src="http://placehold.it/25" alt="Twitter" /></a>
+      <footer className="footer" role="footer">
+        <div className="container">
+          <div className="ten columns">
+            <div className="u-pull-left">
+              <span>Copyright 2016 Brandon Hopkins Studio</span>
+              &nbsp;&nbsp;|&nbsp;&nbsp;
+              <a href="mailto:hello@brandonhopkins.co">Contact</a>
+            </div>
+            <div className="u-pull-right social-links">
+              <a href="https://www.behance.net/brandonhopkins" target="_blank">
+                <img src={ behanceImg } alt="Behance" />
+              </a>
+              <a href="https://dribbble.com/therealmccoydesign" target="_blank">
+                <img src={ dribbleImg } alt="Dribble" />
+              </a>
+              <a href="https://twitter.com/_b_ill" target="_blank">
+                <img src={ twitterImg } alt="Twitter" />
+              </a>
+            </div>
+          </div>
         </div>
       </footer>
     );
