068f4b68bb05d1752610e6dfdd4cc2e1d91f335d
Zachary J Rollyson <zach@izimobile.com>
2016-07-07 22:32:51 -0400

    Remove postCSS config from prod webpack build.

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -36,34 +36,11 @@ var commonLoaders = [
         limit: 10000,
     }
   },
-  { test: /\.css$/,
-    loader: ExtractTextPlugin.extract('style-loader', 'css-loader?module!postcss-loader')
-  },
   {
     test: /\.scss$/,
     loaders: ["style", "css", "sass"]
   }
 ];
-
-var postCSSConfig = function() {
-  return [
-    require('postcss-import')(),
-    // Note: you must set postcss-mixins before simple-vars and nested
-    require('postcss-mixins')(),
-    require('postcss-simple-vars')(),
-    // Unwrap nested rules like how Sass does it
-    require('postcss-nested')(),
-    //  parse CSS and add vendor prefixes to CSS rules
-    require('autoprefixer')({
-      browsers: ['last 2 versions', 'IE > 8']
-    }),
-    // A PostCSS plugin to console.log() the messages registered by other
-    // PostCSS plugins
-    require('postcss-reporter')({
-      clearMessages: true
-    })
-  ];
-};
 
 module.exports = [
   {
@@ -126,8 +103,7 @@ module.exports = [
           __DEVSERVER__: false
         }),
         new InlineEnviromentVariablesPlugin({ NODE_ENV: 'production' })
-    ],
-    postcss: postCSSConfig
+    ]
   }, {
     // The configuration for the server-side rendering
     name: "server-side rendering",
